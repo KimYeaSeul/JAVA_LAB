10737eec3ec30137c4b719ca934775fbbb2c2e93
KimYeaSeul <adgjl3151@hanmail.net>
2021-09-27 02:14:12 +0900

    study downcasting & instanceof

--- Chapter7/src/polymorphism/AnimalTest.java
+++ Chapter7/src/polymorphism/AnimalTest.java
@@ -21,11 +21,17 @@ class Tiger extends Animal{
 	public void move() {
 		System.out.println("호랑이가 네발로 뜁니다.");		
 	}
+	public void hunting() {
+		System.out.println("호랑이가 사냥을 합니다.");		
+	}
 }
 
 class Eagle extends Animal{
 	public void move() {
 		System.out.println("독수리가 하늘을 날아갑니다.");		
+	}
+	public void flying() {
+		System.out.println("독수리가 날개를 쭉 펴고 멀리 날아갑니다.");
 	}
 }
 
@@ -36,6 +42,20 @@ public class AnimalTest {
 		Animal hAnimal = new Human();
 		Animal tAnimal = new Tiger();
 		Animal eAnimal = new Eagle();
+		
+		// 다운캐스팅 후 method 보임
+		if(hAnimal instanceof Human) {
+			Human human = (Human)hAnimal;
+			human.readBooks();
+		}
+		
+		// 휴먼 인스턴스를 이글로 강제 캐스팅 하려고해서 오류가 남.
+		// 컴파일 에서는 자료형 만체크해서 오류 안남. 빌드하면 오류 남.
+//		Eagle human = (Eagle)hAnimal;
+		// 아래처럼 하면 프로그램이 다운되지 않음.(안정적)
+//		if(hAnimal instanceof Human) {
+//			Eagle human = (Eagle)hAnimal;
+//		}
 		
 		/*
 		AnimalTest test = new AnimalTest();
@@ -49,19 +69,41 @@ public class AnimalTest {
 		animalList.add(tAnimal);
 		animalList.add(eAnimal);
 		
+		AnimalTest test = new AnimalTest();
+		test.testDownCasting(animalList);
+		
 		for(Animal animal : animalList) {
 			animal.move();
 			
 		}
 	}
+//	
+//	public void moveAnimal(Animal animal) {
+//		animal.move();
+//	}
+//	
+//	// 오버로딩
+//	public void moveAnimal(Human animal) {
+//		animal.move();
+//	}
 	
-	public void moveAnimal(Animal animal) {
-		animal.move();
-	}
-	
-	// 오버로딩
-	public void moveAnimal(Human animal) {
-		animal.move();
+	public void testDownCasting(ArrayList<Animal> list) {
+		for(int i=0; i<list.size(); i++) {
+			Animal animal = list.get(i);
+
+			if( animal instanceof Human) {
+				Human human = (Human)animal;
+				human.readBooks();
+			}else if( animal instanceof Tiger) {
+				Tiger tiger = (Tiger)animal;
+				tiger.hunting();
+			}else if( animal instanceof Eagle) {
+				Eagle eagle = (Eagle)animal;
+				eagle.flying();
+			}else {
+				System.out.println("error");
+			}
+		}
 	}
 
 }
