d58ec46c82ab0dc7e1aabafa7114b57a0f0b58a3
KimYeaSeul <adgjl3151@hanmail.net>
2022-07-28 15:13:47 +0900

    메인페이지수정-API명세

--- HangHae_W3/src/main/java/com/sparta/week01/controller/BoardApiController.java
+++ HangHae_W3/src/main/java/com/sparta/week01/controller/BoardApiController.java
@@ -20,10 +20,6 @@ public class BoardApiController {
     @Autowired
     private BoardRepository boardRepository;
 
-    @GetMapping("/")
-    public String home(){
-        return "<h1>Welcome to Page</h1><h2>항해 3주차 개인프로젝트 페이지</h2>";
-    }
     // 글쓰기
     @PostMapping("/api/boards")
     public ResponseDto<Board> write(@RequestBody Map<String,String> param){
@@ -64,5 +60,61 @@ public class BoardApiController {
     @PostMapping("/api/boards/auth/{id}")
     public ResponseDto<String> certifyPw(@PathVariable int id, @RequestBody Map<String, String> map){
         return boardService.certifyPw(id, map);
+    }
+
+    @GetMapping("/")
+    public String home(){
+        return "<h1>Welcome to Page</h1><h2>항해 3주차 개인프로젝트 페이지</h2>" +
+                "<table style=\"border-collapse: collapse; width: 100%; height: 108px;\" border=\"1\" data-ke-align=\"alignLeft\">\n" +
+                "<tr style=\"height: 18px;\">\n" +
+                "<td style=\"width: 8.60469%; height: 18px;\">Method</td>\n" +
+                "<td style=\"width: 19.0116%; height: 18px;\">URL</td>\n" +
+                "<td style=\"width: 16.8024%; height: 18px;\">function</td>\n" +
+                "<td style=\"width: 22.5582%; height: 18px;\">Request</td>\n" +
+                "<td style=\"width: 33.0232%; height: 18px;\">Response</td>\n" +
+                "</tr>\n" +
+                "<tr style=\"height: 18px;\">\n" +
+                "<td style=\"width: 8.60469%; height: 18px;\">GET</td>\n" +
+                "<td style=\"width: 19.0116%; height: 18px;\">/api/boards</td>\n" +
+                "<td style=\"width: 16.8024%; height: 18px;\">전체 게시글 목록</td>\n" +
+                "<td style=\"width: 22.5582%; height: 18px;\">-</td>\n" +
+                "<td style=\"width: 33.0232%; height: 18px;\">{<br />&nbsp;&nbsp;&nbsp;&nbsp;\"success\":&nbsp;true,<br />&nbsp;&nbsp;&nbsp;&nbsp;\"data\":&nbsp;[<br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{<br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\"id\":&nbsp;1,<br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\"title\":&nbsp;\"title1\",<br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\"author\":&nbsp;\"kim\",<br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\"password\":&nbsp;\"1234\",<br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\"content\":&nbsp;\"content1\",<br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\"createAt\":&nbsp;\"2022-07-28T05:03:59.022+00:00\",<br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\"modifiedAt\":&nbsp;\"2022-07-28T05:03:59.022+00:00\"<br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;},<br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{<br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\"id\":&nbsp;2,<br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\"title\":&nbsp;\"title2\",<br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\"author\":&nbsp;\"kim\",<br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\"password\":&nbsp;\"1234\",<br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\"content\":&nbsp;\"content2\",<br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\"createAt\":&nbsp;\"2022-07-28T05:15:53.095+00:00\",<br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\"modifiedAt\":&nbsp;\"2022-07-28T05:15:53.095+00:00\"<br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;},<br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{<br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\"id\":&nbsp;3,<br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\"title\":&nbsp;\"title3\",<br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\"author\":&nbsp;\"kim\",<br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\"password\":&nbsp;\"1234\",<br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\"content\":&nbsp;\"content3\",<br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\"createAt\":&nbsp;\"2022-07-28T05:23:32.393+00:00\",<br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\"modifiedAt\":&nbsp;\"2022-07-28T05:23:32.393+00:00\"<br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;}<br />&nbsp;&nbsp;&nbsp;&nbsp;]<br />}</td>\n" +
+                "</tr>\n" +
+                "<tr style=\"height: 18px;\">\n" +
+                "<td style=\"width: 8.60469%; height: 18px;\">GET</td>\n" +
+                "<td style=\"width: 19.0116%; height: 18px;\">/api/boards/{id}</td>\n" +
+                "<td style=\"width: 16.8024%; height: 18px;\">특정 게시글 가져오기</td>\n" +
+                "<td style=\"width: 22.5582%; height: 18px;\">-</td>\n" +
+                "<td style=\"width: 33.0232%; height: 18px;\">{<br />&nbsp;&nbsp;&nbsp;&nbsp;\"success\":&nbsp;true,<br />&nbsp;&nbsp;&nbsp;&nbsp;\"data\":&nbsp;{<br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\"id\":&nbsp;3,<br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\"title\":&nbsp;\"title3\",<br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\"author\":&nbsp;\"kim\",<br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\"password\":&nbsp;\"1234\",<br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\"content\":&nbsp;\"content3\",<br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\"createAt\":&nbsp;\"2022-07-28T05:23:32.393+00:00\",<br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\"modifiedAt\":&nbsp;\"2022-07-28T05:23:32.393+00:00\"<br />&nbsp;&nbsp;&nbsp;&nbsp;}<br />}</td>\n" +
+                "</tr>\n" +
+                "<tr style=\"height: 18px;\">\n" +
+                "<td style=\"width: 8.60469%; height: 18px;\">POST</td>\n" +
+                "<td style=\"width: 19.0116%; height: 18px;\">/api/boards</td>\n" +
+                "<td style=\"width: 16.8024%; height: 18px;\">글쓰기</td>\n" +
+                "<td style=\"width: 22.5582%; height: 18px;\">{<br />\"title\"&nbsp;:&nbsp;\"title3\",<br />\"author\"&nbsp;:&nbsp;\"kim\",<br />\"content\"&nbsp;:&nbsp;\"content3\",<br />\"password\"&nbsp;:&nbsp;\"1234\"<br />}</td>\n" +
+                "<td style=\"width: 33.0232%; height: 18px;\">{<br />&nbsp;&nbsp;&nbsp;&nbsp;\"success\":&nbsp;true,<br />&nbsp;&nbsp;&nbsp;&nbsp;\"data\":&nbsp;{<br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\"id\":&nbsp;3,<br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\"title\":&nbsp;\"title3\",<br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\"author\":&nbsp;\"kim\",<br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\"password\":&nbsp;\"1234\",<br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\"content\":&nbsp;\"content3\",<br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\"createAt\":&nbsp;\"2022-07-28T05:23:32.393+00:00\",<br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\"modifiedAt\":&nbsp;\"2022-07-28T05:23:32.393+00:00\"<br />&nbsp;&nbsp;&nbsp;&nbsp;}<br />}</td>\n" +
+                "</tr>\n" +
+                "<tr style=\"height: 18px;\">\n" +
+                "<td style=\"width: 8.60469%; height: 18px;\">POST</td>\n" +
+                "<td style=\"width: 19.0116%; height: 18px;\">/api/boards/auth/{id}</td>\n" +
+                "<td style=\"width: 16.8024%; height: 18px;\">비밀번호 확인하기</td>\n" +
+                "<td style=\"width: 22.5582%; height: 18px;\">password : 1234<br /><br />password : 12345</td>\n" +
+                "<td style=\"width: 33.0232%; height: 18px;\">{<br />&nbsp;&nbsp;&nbsp;&nbsp;\"success\":&nbsp;true,<br />&nbsp;&nbsp;&nbsp;&nbsp;\"data\":&nbsp;\"비밀번호가&nbsp;일치합니다.\"<br />}<br /><br />{<br />&nbsp;&nbsp;&nbsp;&nbsp;\"success\":&nbsp;false,<br />&nbsp;&nbsp;&nbsp;&nbsp;\"data\":&nbsp;\"비밀번호가&nbsp;일치하지&nbsp;않습니다.\"<br />}</td>\n" +
+                "</tr>\n" +
+                "<tr style=\"height: 18px;\">\n" +
+                "<td style=\"width: 8.60469%; height: 18px;\">PUT</td>\n" +
+                "<td style=\"width: 19.0116%; height: 18px;\">/api/boards/{id}</td>\n" +
+                "<td style=\"width: 16.8024%; height: 18px;\">특정 게시글 수정하기</td>\n" +
+                "<td style=\"width: 22.5582%; height: 18px;\">{<br />&nbsp;&nbsp;&nbsp;&nbsp;\"title\"&nbsp;:&nbsp;\"title3&nbsp;update\",<br />&nbsp;&nbsp;&nbsp;&nbsp;\"author\"&nbsp;:&nbsp;\"kim\",<br />&nbsp;&nbsp;&nbsp;&nbsp;\"content\"&nbsp;:&nbsp;\"content3&nbsp;update\"<br />}</td>\n" +
+                "<td style=\"width: 33.0232%; height: 18px;\">{<br />&nbsp;&nbsp;&nbsp;&nbsp;\"success\":&nbsp;true,<br />&nbsp;&nbsp;&nbsp;&nbsp;\"data\":&nbsp;{<br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\"id\":&nbsp;3,<br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\"title\":&nbsp;\"title3&nbsp;update\",<br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\"author\":&nbsp;\"kim\",<br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\"password\":&nbsp;\"1234\",<br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\"content\":&nbsp;\"content3&nbsp;update\",<br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\"createAt\":&nbsp;\"2022-07-28T05:23:32.393+00:00\",<br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\"modifiedAt\":&nbsp;\"2022-07-28T05:27:19.442+00:00\"<br />&nbsp;&nbsp;&nbsp;&nbsp;}<br />}</td>\n" +
+                "</tr>\n" +
+                "<tr>\n" +
+                "<td style=\"width: 8.60469%;\">DELETE</td>\n" +
+                "<td style=\"width: 19.0116%;\">/api/boards/{id}</td>\n" +
+                "<td style=\"width: 16.8024%;\">특정 게시글 삭제하기</td>\n" +
+                "<td style=\"width: 22.5582%;\">-</td>\n" +
+                "<td style=\"width: 33.0232%;\">{<br />&nbsp;&nbsp;&nbsp;&nbsp;\"success\":&nbsp;true,<br />&nbsp;&nbsp;&nbsp;&nbsp;\"data\":&nbsp;2<br />}</td>\n" +
+                "</tr>\n" +
+                "</table>";
     }
 }
