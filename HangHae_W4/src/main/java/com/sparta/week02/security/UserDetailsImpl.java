b1b5c2351a71581eb6cd0e824e49e434ebb66e6e
KimYeaSeul <adgjl3151@hanmail.net>
2022-08-01 22:06:18 +0900

    create product

---
+++ HangHae_W4/src/main/java/com/sparta/week02/security/UserDetailsImpl.java
@@ -0,0 +1,54 @@
+package com.sparta.week02.security;
+
+import com.sparta.week02.model.Users;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+
+@RequiredArgsConstructor
+public class UserDetailsImpl implements UserDetails {
+
+    public final Users user;
+
+    public Users getUser(){
+        return user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
