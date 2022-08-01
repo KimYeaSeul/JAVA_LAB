1fef7ea1d3de5dc965c461adabe88ed253870934
KimYeaSeul <adgjl3151@hanmail.net>
2022-08-01 14:15:03 +0900

    비밀번호 암호화

--- HangHae_W4/src/main/java/com/sparta/week02/security/WebSecurityConfig.java
+++ HangHae_W4/src/main/java/com/sparta/week02/security/WebSecurityConfig.java
@@ -4,19 +4,32 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
+
+    @Bean
+    public BCryptPasswordEncoder encodePassword(){
+        return new BCryptPasswordEncoder();
+    }
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer(){
+        return (web) -> web.ignoring()
+                .antMatchers("/h2-console/**");
+    }
     @Bean
     public SecurityFilterChain filterChian(HttpSecurity http) throws Exception {
+        http.csrf().ignoringAntMatchers("/user/**");
         http.authorizeHttpRequests((authz) -> authz
                         .antMatchers("/image/**")
                         .permitAll()
                         .antMatchers("/css/**")
                         .permitAll()
-                        .antMatchers("/user/signup")
+                        .antMatchers("/user/**")
                         .permitAll()
                         .anyRequest().authenticated()
                 )
