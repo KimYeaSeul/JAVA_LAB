ee7cf01518de36276ad885c4b2cdb0a902c1d5bc
KimYeaSeul <adgjl3151@hanmail.net>
2022-08-05 05:37:51 +0900

    Security Config 수정 : 허용 주소 변수화

--- HangHae_W4/src/main/java/com/sparta/week02/security/WebSecurityConfig.java
+++ HangHae_W4/src/main/java/com/sparta/week02/security/WebSecurityConfig.java
@@ -41,10 +41,14 @@ public class WebSecurityConfig{
             /* swagger v3 */
             "/v3/api-docs/**",
             "/swagger-ui/**",
-            "/swagger"
+            "/swagger",
+            /* static and unnecessary Authehtication*/
+            "/image/**",
+            "/css/**",
+            "/user/**",
+            "/boards/**",
+            "/board/**"
     };
-//    private final UserDetailsServiceImpl userDetailsService;
-//    private final JwtAuthehnticationFilter jwtAuthehnticationFilter;
     @Bean
     public BCryptPasswordEncoder encodePassword(){
         return new BCryptPasswordEncoder();
@@ -55,43 +59,31 @@ public class WebSecurityConfig{
                 .antMatchers("/h2-console/**");
     }
 
-        @Bean
-        public AuthenticationManager authenticationManager() throws Exception {
-            return authenticationManager.getAuthenticationManager();
-        }
+    @Bean
+    public AuthenticationManager authenticationManager() throws Exception {
+        return authenticationManager.getAuthenticationManager();
+    }
 
+    @Bean
+    public TokenProvider TokenProvider() {
+        return new TokenProvider();
+    }
 
-//        private final AuthenticationManager auth;
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 //        http.addFilterBefore(new MyFilter1(), SecurityContextHolderFilter.class);
         http.authorizeHttpRequests((authz) -> authz
-                        .antMatchers("/image/**")
-                        .permitAll()
-                        .antMatchers("/css/**")
-                        .permitAll()
-                        .antMatchers("/user/**")
-                        .permitAll()
-                        .antMatchers("/boards/**")
-                        .permitAll()
                         .antMatchers(PERMIT_URL_ARRAY)
                         .permitAll()
                         .anyRequest().authenticated()
                 )
                 .formLogin().disable()
                 .addFilter(corsFilter)
-//                .authenticationManager(authenticationManager)
                 .addFilterBefore( new JwtAuthehnticationFilter(authenticationManager(), TokenProvider(), refreshTokenRepository), UsernamePasswordAuthenticationFilter.class)
                 .addFilterBefore( new JwtAuthorizationFilter(authenticationManager(), userRepository, TokenProvider()), UsernamePasswordAuthenticationFilter.class);
-//                .addFilter(new JwtAuthehnticationFilter(authenticationManager()));
-//
+
         return http.build();
     }
-    @Bean
-    public TokenProvider TokenProvider() {
-        return new TokenProvider();
-    }
-
 }
